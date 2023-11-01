9c7235cedce04f1ba85b3acb0dd8ff38b827d64f
Ismartdesigns <michaelchideradickson18@gmail.com>
2023-11-01 17:32:28 +0100

    add things

--- instructor/dashboard.php
+++ instructor/dashboard.php
@@ -1,50 +1,69 @@
 <?php
 require "../includes/functions.php";
-if(!isset($_SESSION['instructor'])) {
+if (!isset($_SESSION['instructor'])) {
     header("location: ../auth/instructor-login.php");
     exit();
 }
+
+$instructor = $_SESSION["instructor"]; // Use $_SESSION["instructor"] instead of $_SESSION["user"]
+$instructorProfile = fetchInstructorProfile($instructor);
+
+if (!$instructorProfile) {
+    // Handle the case where instructor details are not found
+    echo "Instructor details not found.";
+    exit();
+}
 ?>
+
 <!DOCTYPE html>
 <html class="no-js" lang="en">
 
 <?php include("../includes/dashboard/dash-head.php") ?>
 
-
 <body class="dashboard-page dashboard-nav-fixed">
-
     <!-- Dashboard Nav Start -->
     <?php include('../includes/dashboard/instructor-dash-nav.php') ?>
     <!-- Dashboard Nav End -->
 
     <!-- Dashboard Main Wrapper Start -->
     <main class="dashboard-main-wrapper">
-
         <!-- Dashboard Header Start -->
         <?php include("../includes/dashboard/instructor-dash-header.php") ?>
         <!-- Dashboard Header End -->
 
-
         <!-- Dashboard Content Start -->
         <div class="dashboard-content">
-
             <div class="container">
                 <h4 class="dashboard-title">Dashboard</h4>
 
                 <!-- Dashboard Info Start -->
                 <div class="dashboard-info">
                     <div class="row gy-2 gy-sm-6">
-                        
-                    <div class="col-md-4 col-sm-6">
+                        <div class="col-md-6 col-sm-6">
                             <!-- Dashboard Info Card Start -->
-                            <div class="dashboard-info__card">
-                                <div class="dashboard-info__card-box">
-                                    <div class="dashboard-info__card-icon icon-color-04">
-                                        <i class="edumi edumi-group"></i>
+                            <div class="dashboard-profile">
+                                <div class="dashboard-profile__item">
+                                    <div class="dashboard-profile__heading">First Name</div>
+                                    <div class="dashboard-profile__content">
+                                        <p><?php echo $instructorProfile["firstname"]; ?></p>
                                     </div>
-                                    <div class="dashboard-info__card-content">
-                                        <div class="dashboard-info__card-value">146</div>
-                                        <div class="dashboard-info__card-heading">Total Students</div>
+                                </div>
+                                <div class="dashboard-profile__item">
+                                    <div class="dashboard-profile__heading">Last Name</div>
+                                    <div class="dashboard-profile__content">
+                                        <p><?php echo $instructorProfile["lastname"]; ?></p>
+                                    </div>
+                                </div>
+                                <div class="dashboard-profile__item">
+                                    <div class="dashboard-profile__heading">Username</div>
+                                    <div class="dashboard-profile__content">
+                                        <p><?php echo $instructorProfile["username"]; ?></p>
+                                    </div>
+                                </div>
+                                <div class="dashboard-profile__item">
+                                    <div class="dashboard-profile__heading">Email</div>
+                                    <div class="dashboard-profile__content">
+                                        <p><?php echo $instructorProfile["email"]; ?></p>
                                     </div>
                                 </div>
                             </div>
@@ -65,38 +84,14 @@ if(!isset($_SESSION['instructor'])) {
                             </div>
                             <!-- Dashboard Info Card End -->
                         </div>
-                        <div class="col-md-4 col-sm-6">
-                            <!-- Dashboard Info Card Start -->
-                            <div class="dashboard-info__card">
-                                <div class="dashboard-info__card-box">
-                                    <div class="dashboard-info__card-icon icon-color-06">
-                                        <i class="edumi edumi-coin"></i>
-                                    </div>
-                                    <div class="dashboard-info__card-content">
-                                        <div class="dashboard-info__card-value"><span class="sale-price">$383.<small class="separator">01</small></span></div>
-                                        <div class="dashboard-info__card-heading">Total Earnings</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Dashboard Info Card End -->
-                        </div>
                     </div>
                 </div>
                 <!-- Dashboard Info End -->
             </div>
-
-
         </div>
         <!-- Dashboard Content End -->
-
-
     </main>
     <!-- Dashboard Main Wrapper End -->
-
-
-
-
-
     <!-- JS Vendor, Plugins & Activation Script Files -->
 
     <!-- Vendors JS -->
@@ -118,13 +113,3 @@ if(!isset($_SESSION['instructor'])) {
     <script src="../assets/js/plugins/masonry.pkgd.min.js"></script>
     <script src="../assets/js/plugins/flatpickr.js"></script>
     <script src="../assets/js/plugins/range-slider.js"></script>
-    <script src="../assets/js/plugins/select2.min.js"></script>
-
-
-    <!-- Activation JS -->
-    <script src="../assets/js/main.js"></script>
-
-
-</body>
-
-</html>
